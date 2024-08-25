f1de697b46a9ace40ab81cbba5aef4f6193b3981
Ritik Porwal <ritikporwal710@gmail.com>
2024-08-25 23:17:54 +0530

    25 aug 2024

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import './FoodDisplay.css';
 import { food_list } from './../../assets/assets';
 import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
 
 
 const FoodDisplay = () => {
@@ -13,7 +14,7 @@ const FoodDisplay = () => {
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index) =>{
-                return
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
             })}
         </div>
     </div>
