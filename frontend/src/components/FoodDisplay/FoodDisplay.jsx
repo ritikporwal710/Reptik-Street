a4380dcb73cefc5a423ac0d692fe682391646c39
Ritik Porwal <ritikporwal710@gmail.com>
2024-08-28 21:32:25 +0530

    28 aug 2024

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -5,7 +5,7 @@ import { StoreContext } from '../../context/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
 
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
 
     const {food_list} = useContext(StoreContext)    
 
@@ -13,9 +13,15 @@ const FoodDisplay = () => {
     <div className='food-display' id='food-display'>
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
-            {food_list.map((item,index) =>{
+            {
+            food_list.map((item,index) =>{
+              {console.log(category,index);}
+              if (category=="All" || category===item.category) {
                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-            })}
+              }
+                
+            })
+            }
         </div>
     </div>
   )
