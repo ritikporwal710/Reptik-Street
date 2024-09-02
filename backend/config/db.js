1a99ab526b0a35d47b72460a4f622a239f36e850
Ritik Porwal <ritikporwal710@gmail.com>
2024-09-02 22:41:00 +0530

    02 sep 2024

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,4 +3,5 @@ import mongoose from "mongoose";
 export const connectDB = async() =>{
     await mongoose.connect('mongodb+srv://rishu8840:8423610029@cluster0.j3aux.mongodb.net/Reptik').
     then(()=> console.log("DB Connection Successfully"));
-}
+    
+}   
