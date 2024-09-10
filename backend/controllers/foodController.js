b936019fc9fdcd6cc49c1126216b377bc101497c
Ritik Porwal <ritikporwal710@gmail.com>
2024-09-10 23:19:42 +0530

    10 sep 2024

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -13,6 +13,7 @@ const addFood = async(req,res) =>{
         price: req.body.price,
         category: req.body.category,
         image: image_filename
+        
     })
     try {
         await food.save();
