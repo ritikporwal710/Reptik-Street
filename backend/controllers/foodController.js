02fff89259abb53c10d0a390e8d12478280af169
Ritik Porwal <ritikporwal710@gmail.com>
2024-09-11 23:30:25 +0530

    11 sep 2024

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,33 +1,31 @@
 import foodModel from "../models/foodModel.js";
-import fs from 'fs';
-
+import fs from "fs";
 
 // add food item through API
 
-const addFood = async(req,res) =>{
-    let image_filename = `${req.file.filename}`;
+const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
 
-    const food = new foodModel({
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-        category: req.body.category,
-        image: image_filename
-        
-    })
-    try {
-        await food.save();
-        res.json({
-            success: true,
-            message: "Food Added"
-        })
-    } catch (error) {
-        console.log(error)
-        res.json({
-            success: false,
-            message: "Error "
-        })
-    }
-}
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+  try {
+    await food.save();
+    res.json({
+      success: true, 
+      message: "Food Added",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Error ",
+    });
+  }
+};
 
-export {addFood};
+export { addFood };
