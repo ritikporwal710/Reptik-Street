3814e0adc392d88d509903de9c5747220eae8bed
Ritik Porwal <ritikporwal710@gmail.com>
2025-03-20 01:14:21 +0530

    removed node modules

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -9,12 +9,20 @@ const addFood = async (req, res) => {
 
   let image_filename = req.file.filename;
 
+  // const food = new foodModel({
+  //   name: req.body.name,
+  //   description: req.body.description,
+  //   price: req.body.price,
+  //   category: req.body.category,
+  //   image: image_filename,
+  // });
+
   const food = new foodModel({
     name: req.body.name,
     description: req.body.description,
     price: req.body.price,
     category: req.body.category,
-    image: image_filename,
+    image: req.file.path, // Cloudinary URL
   });
   try {
     await food.save();
