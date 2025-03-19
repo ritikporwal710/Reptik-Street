7f772adb35a47ed7522450b809897654a1a0917e
Ritik Porwal <ritikporwal710@gmail.com>
2025-03-20 00:49:41 +0530

    used-cloudinary-to-upload-images

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -5,19 +5,28 @@ import {
   removeFood,
 } from "../controllers/foodController.js";
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../utils/cloudinaryConfig.js";
 
 const foodRouter = express.Router();
 
 // Image storage engine
 
-const storage = multer.diskStorage({
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`);
+// const storage = multer.diskStorage({
+//   destination: "uploads",
+//   filename: (req, file, cb) => {
+//     return cb(null, `${Date.now()}${file.originalname}`);
+//   },
+// });
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads", // Keeps the same folder structure
+    format: async (req, file) => "png", // Convert all to PNG
+    public_id: (req, file) => `${Date.now()}-${file.originalname}`,
   },
 });
-
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 foodRouter.post("/add", upload.single("image"), addFood);
 
